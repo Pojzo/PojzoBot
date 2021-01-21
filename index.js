67fce5b374dba59a304993361ec01f924a14b13a
Peter Kovac <pojzinko8@gmail.com>
2021-01-21 17:15:18 +0100

    Basic login

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("TESTING");
+const Discord = require('discord.js');
+const keys = require('./keys.json');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log("PojzoBot is online!");
+})
+
+var token = keys.token;
+client.login(token);
